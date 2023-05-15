ce6d27a9202193a0a866182c38143c945695f994
estiven2111 <estiven.a.martinez@gmail.com>
2023-05-15 15:37:24 -0500

    url produccion

--- src/env.js
+++ src/env.js
@@ -1,4 +1,4 @@
-const URLS = "http://localhost:3001";
+const URLS = "https://pruebaback-production-0050.up.railway.app";
 // REMOTA = 'https://pruebaback-production-0050.up.railway.app';
 
 module.exports = {URLS}
