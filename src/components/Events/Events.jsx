9dd6c82222afd1a2ac761fb6c6fa9dc5439c8a67
estiven2111 <estiven.a.martinez@gmail.com>
2023-05-17 13:39:01 -0500

    deploy12

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -144,7 +144,7 @@ const Events = ({ showFilters }) => {
       <br/>
       <Paginado events={events.length} eventsPerPage={eventsPerPage} />
       <div className={style.containerHelp}>
-        {events && events.map((item, index) => {
+        {currentEvents && currentEvents.map((item, index) => {
           if (islogin.isAuthenticated) {
             if (islogin.user.id !== item.id_Artist) {
               return (
