087233d10c940d61f8340d52b0bde938ce67c434
estiven2111 <estiven.a.martinez@gmail.com>
2023-05-16 19:43:25 -0500

    BTN GOOGLE

--- src/components/GoogleButton/googleButton.jsx
+++ src/components/GoogleButton/googleButton.jsx
@@ -12,12 +12,15 @@ const LoginButton = () => {
     console.log("entre a fetchaurh");
     // const response = await axios.get('https://pruebaback-production-0050.up.railway.app/artist/auth/user', {withCredentials:true}).catch((err) => {
     try {
+      const res = await axios.get("/artist")
+      console.log(res);
+
       let response = await axios.get('/artist/auth/user', {withCredentials:true})
       // fetch('https://pruebaback-production-0050.up.railway.app/artist/auth/user',{withCredentials:true})
       // .then(res =>{
       //   console.log(res);
       // })
-     
+     console.log(response);
       if (response && response.data) {
         const token = response.data
         dispatch(loginSuccess({ token }))
@@ -33,6 +36,7 @@ const LoginButton = () => {
   const handleLogin = async () => {
     let timer = null;
     const googleLoginURL = `${URLS}/artist/auth/google`
+    console.log(googleLoginURL);
     // const googleLoginURL = "https://pruebaback-production-0050.up.railway.app/artist/auth/google"
     const newWindow = window.open(googleLoginURL,"_blank","width=350,height=450")
 
