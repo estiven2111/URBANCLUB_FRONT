a5f949ea259971611aaa4d8a4fd0998a518e372d
estiven2111 <estiven.a.martinez@gmail.com>
2023-05-18 12:16:46 -0500

    forgotpassword

--- src/components/ForgotPassword/ForgotPassword.jsx
+++ src/components/ForgotPassword/ForgotPassword.jsx
@@ -14,26 +14,61 @@ const ForgotPassword = () => {
     });
   };
 
-  const submitHandler = (e) => {
+  const submitHandler = async (e) => {
     e.preventDefault();
-    // axios.put("https://pruebaback-production-0050.up.railway.app/artist/forgotPassword", input);
-    axios.put("/artist/forgotPassword", input);
-    setInput({
-      email: "",
-    });
-    swal({
-      title: "Recuperación de contraseña",
-      text: "Se ha enviado el correo electrónico",
-      icon: "success",
-      buttons: "Aceptar",
-    });
+
+    if (input.email === "") {
+      swal({
+        title: "Campo vacío",
+        text: "Por favor, ingresa tu correo electrónico.",
+        icon: "error",
+        buttons: "Aceptar",
+      });
+      return;
+    }
+
+    try {
+      // Aquí debes hacer la llamada a la API para verificar si el correo electrónico existe
+      const response = await axios.put("/artist/forgotPassword", input);
+
+      // Si el correo electrónico no existe, puedes mostrar una alerta
+      if (response.data.emailExists === false) {
+        swal({
+          title: "Correo electrónico inválido",
+          text: "El correo electrónico ingresado no existe.",
+          icon: "error",
+          buttons: "Aceptar",
+        });
+        return;
+      }
+
+      // Si el correo electrónico existe y la solicitud fue exitosa
+      setInput({
+        email: "",
+      });
+      swal({
+        title: "Recuperación de contraseña",
+        text: "Se ha enviado el correo electrónico",
+        icon: "success",
+        buttons: "Aceptar",
+      });
+    } catch (error) {
+      // Manejo de errores de la llamada a la API
+      console.error(error);
+      swal({
+        title: "Error",
+        text: "Ocurrió un error al enviar la solicitud.",
+        icon: "error",
+        buttons: "Aceptar",
+      });
+    }
   };
 
   return (
     <div className={style.container}>
       <form className={style.form} onSubmit={submitHandler}>
         <label className={style.label}>Ingresa tu correo electrónico</label>
-        <input 
+        <input
           className={style.input}
           type="text"
           name="email"
