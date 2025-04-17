f0ecdb48e20a1add7119f0e45b71a4da320ae451
estiven2111 <estiven.a.martinez@gmail.com>
2025-04-16 19:57:51 -0500

    urbanclub

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,9 @@ import style from "./Footer.module.css";
 import gglftr from "./img/ggl-ftr.png";
 import fbftr from "./img/fb-ftr.png";
 import FacebookIcon from "@mui/icons-material/Facebook";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import LanguageIcon from '@mui/icons-material/Language';
 import GoogleIcon from "@mui/icons-material/Google";
 import {EMAIL_ADDRES} from "../../env"
 
@@ -61,7 +64,7 @@ const Footer = () => {
       >
         <h3>Contactenos</h3>
         <a
-          href="https://www.facebook.com/profile.php?id=100068893039514"
+          href="https://github.com/estiven2111"
           target="_blank"
           rel="noreferrer"
           style={{
@@ -70,7 +73,7 @@ const Footer = () => {
             color: "white",
           }}
         >
-          <FacebookIcon
+          <GitHubIcon
             style={{
               fontSize: "50px",
               color: "#ccf1fd",
@@ -78,11 +81,38 @@ const Footer = () => {
           />
         </a>
         <a
-          href= {`${EMAIL_ADDRES}`}
+          href="https://www.linkedin.com/in/estiven-arboleda-marinez-bb9aa61a4"
           target="_blank"
           rel="noreferrer"
+          style={{
+            cursor: "pointer",
+            fontSize: "50px",
+            color: "white",
+          }}
         >
-          <GoogleIcon style={{ fontSize: "50px", color: "#ccf1fd" }} />
+          <LinkedInIcon
+            style={{
+              fontSize: "50px",
+              color: "#ccf1fd",
+            }}
+          />
+        </a>
+        <a
+          href="https://portafolio-estiven.web.app"
+          target="_blank"
+          rel="noreferrer"
+          style={{
+            cursor: "pointer",
+            fontSize: "50px",
+            color: "white",
+          }}
+        >
+          <LanguageIcon
+            style={{
+              fontSize: "50px",
+              color: "#ccf1fd",
+            }}
+          />
         </a>
       </div>
     </div>
